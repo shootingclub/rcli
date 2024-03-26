864708b4d6a08d50aa54c45dd5a2daa713e76ae9
张耀华 <13752354704@163.com>
2024-03-26 10:48:35 +0800

    Option 包装联系

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
+
 fn main() {
     println!("Hello, world!");
     let a = 1;
-    add(a);
-    println!("{}", a);
+    let b = 2;
+    let rslt: Option<i32> = add(a, b);
+    println!("{},{},{:?}", a, b, rslt.unwrap());
 }
 
-fn add(x: i32) -> i32 {
-    let b = x;
-    b
+fn add(x: i32, y: i32) -> Option<i32> {
+    Some(x + y)
 }
