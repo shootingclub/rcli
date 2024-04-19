2e0dc90fde315c46076e053e2945d09109414802
张耀华 <13752354704@163.com>
2024-04-19 16:24:13 +0800

    v1.1

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,27 @@
-fn main() {
-    println!("Hello, world!");
-    let a = 1;
-    let b = 2;
-    let rslt: Option<i32> = add(a, b);
-    println!("{},{},{:?}", a, b, rslt.unwrap());
-}
+// rcli csv -i input.csv -o output.json --header -d ','
 
-fn add(x: i32, y: i32) -> Option<i32> {
-    Some(x + y)
+use clap::Parser;
+use rcli::{process_csv, Opts, SubCommand};
+
+fn main() -> anyhow::Result<()> {
+    let opts = Opts::parse();
+    if let Some(name) = opts.name.as_ref() {
+        println!("name: {}", name);
+    }
+
+    if let Some(config_path) = opts.config.as_deref() {
+        println!("config: {}", config_path.display());
+    }
+
+    match opts.debug {
+        0 => println!("Debug mode is off"),
+        1 => println!("Debug mode is kind of on"),
+        2 => println!("Debug mode is on"),
+        _ => println!("Don't be crazy"),
+    }
+
+    match opts.cmd {
+        SubCommand::Csv(opts) => process_csv(&opts.input, &opts.output)?,
+    }
+    Ok(())
 }
